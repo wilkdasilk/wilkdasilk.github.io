1bb20a805bbbb5ca5624bb2b7bdfcbda84516da5
wilkdasilk <anwilk92@gmail.com>
2017-06-29 13:03:09 -0700

    parallax dividers

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,59 +16,75 @@ String.prototype.titleize = function() {
 }
 
 var tempProject = {};
-var showProjectIndex = 0;
-var currentProject;
+//var showProjectIndex = 0;
+var projectCount = 0;
+//var currentProject;
 
-$.ajax({
-  url: "https://api.github.com/search/repositories?q=user:wilkdasilk+topic:featured+fork:true&sort=updated",
-  method: "GET",
-  success: getProjects,
-  error: onError
-});
+// $.ajax({
+//   url: "https://api.github.com/search/repositories?q=user:wilkdasilk+topic:featured+fork:true&sort=updated",
+//   method: "GET",
+//   success: getProjects,
+//   error: onError
+// });
 
 //json for projects
+
+var imageLinks = [
+  "assets/imgs/downtown-la.jpg",
+  "assets/imgs/angel-city-brewery.jpg",
+  "assets/imgs/pasadena-hike.jpg",
+  "assets/imgs/grand-central-market.jpg",
+  "assets/imgs/santa-monica-pier.jpg",
+  "assets/imgs/mixing-board.jpg"
+]
+
 var data = [
-  // {
-  //   description: "What kind of creature are you? Idk, better take a quiz. Single-page app, no framework.",
-  //   link: "https://github.com/wilkdasilk/MiddleEarthCreatureQuiz",
-  //   live: "https://desolate-wave-59412.herokuapp.com/",
-  //   name: "MiddleEarthCreatureQuiz",
-  //   picture: "https://raw.githubusercontent.com/wilkdasilk/MiddleEarthCreatureQuiz/master/app/assets/images/preview/creaturequiz-preview.png"
-  // },
-  // {
-  //   description: "A travel community for users to share city specific tips about their favorite locations around the world ",
-  //   link: "https://github.com/wilkdasilk/vagabond",
-  //   live: "http://levagabond.herokuapp.com/",
-  //   name: "vagabond",
-  //   picture: "https://raw.githubusercontent.com/wilkdasilk/vagabond/master/app/assets/images/preview/levagabond-preview.png"
-  // },
-  // {
-  //   description: "An attempt to make the world a better place by providing people with cool activities to do",
-  //   link: "https://github.com/wilkdasilk/actividay",
-  //   live: "http://actividay.herokuapp.com/",
-  //   name: "actividay",
-  //   picture: "https://raw.githubusercontent.com/wilkdasilk/actividay/master/app/assets/images/preview/actividay-preview.png"
-  // },
-  // {
-  //   description: "Curate crowdsourced playlists around a geolocation",
-  //   link: "https://github.com/wilkdasilk/NoiseFlag",
-  //   live: "http://noiseflag.herokuapp.com/",
-  //   name: "NoiseFlag",
-  //   picture: "https://raw.githubusercontent.com/wilkdasilk/NoiseFlag/master/app/assets/images/preview/Noiseflag_preview.png"
-  // },
-  // {
-  //   description: "Chat and streaming app for podcasters, enabling early feedback from subscribers via live listening",
-  //   link: "https://github.com/wilkdasilk/PodBooth",
-  //   live: "https://podbooth.herokuapp.com/",
-  //   name: "PodBooth",
-  //   picture: "https://raw.githubusercontent.com/wilkdasilk/PodBooth/master/app/assets/images/preview/podbooth_preview.png"
-  // }
+  {
+    description: "What kind of creature are you? Idk, better take a quiz. Single-page app, no framework.",
+    link: "https://github.com/wilkdasilk/MiddleEarthCreatureQuiz",
+    live: "https://desolate-wave-59412.herokuapp.com/",
+    name: "MiddleEarthCreatureQuiz",
+    picture: "https://raw.githubusercontent.com/wilkdasilk/MiddleEarthCreatureQuiz/master/app/assets/images/preview/creaturequiz-preview.png"
+  },
+  {
+    description: "A travel community for users to share city specific tips about their favorite locations around the world ",
+    link: "https://github.com/wilkdasilk/vagabond",
+    live: "http://levagabond.herokuapp.com/",
+    name: "vagabond",
+    picture: "https://raw.githubusercontent.com/wilkdasilk/vagabond/master/app/assets/images/preview/levagabond-preview.png"
+  },
+  {
+    description: "An attempt to make the world a better place by providing people with cool activities to do",
+    link: "https://github.com/wilkdasilk/actividay",
+    live: "http://actividay.herokuapp.com/",
+    name: "actividay",
+    picture: "https://raw.githubusercontent.com/wilkdasilk/actividay/master/app/assets/images/preview/actividay-preview.png"
+  },
+  {
+    description: "Curate crowdsourced playlists around a geolocation",
+    link: "https://github.com/wilkdasilk/NoiseFlag",
+    live: "http://noiseflag.herokuapp.com/",
+    name: "NoiseFlag",
+    picture: "https://raw.githubusercontent.com/wilkdasilk/NoiseFlag/master/app/assets/images/preview/Noiseflag_preview.png"
+  },
+  {
+    description: "Chat and streaming app for podcasters, enabling early feedback from subscribers via live listening",
+    link: "https://github.com/wilkdasilk/PodBooth",
+    live: "https://podbooth.herokuapp.com/",
+    name: "PodBooth",
+    picture: "https://raw.githubusercontent.com/wilkdasilk/PodBooth/master/app/assets/images/preview/podbooth_preview.png"
+  }
 ];
 
 //body hides
 $('body').hide();
 appendLinks();
-//showProject(data[0]);
+appendProject(data[4]);
+appendProject(data[3]);
+appendProject(data[2]);
+appendProject(data[1]);
+appendProject(data[0]);
+
 
 function appendLinks() {
   console.log("appending links");
@@ -79,24 +95,34 @@ function appendLinks() {
   };
 }
 
-function showProject(project) {
-  $('.projects').html(`
-    <div class='col-lg-6 col-md-6 col-sm-12 col-md-xs-12'>
-      <img src='${project.picture}' class='preview'>
-    </div>
-    <div class='col-lg-6 col-md-6 col-sm-12 col-md-xs-12'>
-      <section>
-        <div class='project'>
-          <h4>${project.name.titleize()}</h3>
-          <p>${project.description}</p>
-          <div class="project-links">
-            <a href='${project.link}' target='_blank'>Repo</a>
-            <a href='${project.live}' target='_blank'>Live</a>
+function appendProject(project) {
+  $('.projects').append(`
+    <div class="">
+      <div class="parallax-container">
+        <div class="parallax"><img src="${imageLinks[projectCount]}"></div>
+      </div>
+      <div class="project-details">
+        <div class="container">
+          <div class='col-lg-6 col-md-6 col-sm-12 col-md-xs-12'>
+            <img src='${project.picture}' class='preview'>
+          </div>
+          <div class='col-lg-6 col-md-6 col-sm-12 col-md-xs-12'>
+            <section>
+              <div class='project'>
+                <h4>${project.name.titleize()}</h3>
+                <p>${project.description}</p>
+                <div class="project-links">
+                  <a href='${project.link}' target='_blank'>Repo</a>
+                  <a href='${project.live}' target='_blank'>Live</a>
+                </div>
+              </div>
+            </section>
           </div>
         </div>
-      </section>
+      </div>
     </div>`
   );
+  projectCount++;
 }
 
 function appendLink(currentProject, index) {
@@ -131,7 +157,7 @@ function addPhoto(res) {
   data.push(tempProject);
   currentProject = data.slice(-1)[0];
   appendLink(currentProject, data.length-1);
-  showProject(data[showProjectIndex]);
+  appendProject(data[data.length - 1]);
   tempProject = {};
 }
 
@@ -142,17 +168,18 @@ function onError(res) {
 //content fades in on page ready
 $(document).ready(function(){
   $('body').fadeIn(800)
+  $('.parallax').parallax();
 });
 
-$('.projects-nav').on('click', 'a', function(event){
-  if (  showProjectIndex != $(this).attr('data-index') ) {
-    showProjectIndex = $(this).attr('data-index');
-    $('.projects-wrapper').children().fadeOut(100, function (){
-      $('.projects-wrapper').children().fadeIn(800);
-    showProject(data[showProjectIndex]);
-    });
-  }
-});
+// $('.projects-nav').on('click', 'a', function(event){
+//   if (  showProjectIndex != $(this).attr('data-index') ) {
+//     showProjectIndex = $(this).attr('data-index');
+//     $('.projects-wrapper').children().fadeOut(100, function (){
+//       $('.projects-wrapper').children().fadeIn(800);
+//     showProject(data[showProjectIndex]);
+//     });
+//   }
+// });
 
 $(window).resize(function(){
  if ($(window).width()>767) {
